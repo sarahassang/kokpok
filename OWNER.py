0a368928d0067a5add58e4b1e074f16531d5ccd1
Mahmoud1269 <98558755+Mahmoud1269@users.noreply.github.com>
2024-01-28 03:04:31 +0200

    Add files via upload

---
+++ OWNER.py
@@ -0,0 +1,10 @@
+# متطلبات التنصيب #
+OWNER = ["Zo_Mbi_e"]
+OWNER_NAME = "zombie"
+BOT_TOKEN = "6022370529:AAHM5anvuHDDhKN52ypmcoKTLWnWFEPVDDE"
+DATABASE = "mongodb+srv://Elkber:Elkber@cluster0.feuljpn.mongodb.net/?retryWrites=true&w=majority"
+CHANNEL = "https://t.me/zombieyy"
+GROUP = "https://t.me/zombie2"
+VIDEO = "https://telegra.ph/file/770e2d5df0b50264097b2.jpg"
+LOGS = "zombie2"
+
